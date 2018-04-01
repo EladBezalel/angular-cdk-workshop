041e237c8e50fef6dd05577ce2f9046e51af701b
Elad Bezalel <elad.bezalel@gmail.com>
2018-04-01 21:19:46 +0300

    Step 3

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,10 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.css']
+  styleUrls: ['./color-picker.component.css'],
+  exportAs: 'colorPicker'
 })
 export class ColorPickerComponent {
   @Input() colors: object[];
+
+  @ViewChild(TemplateRef) template: TemplateRef<any>;
 }
