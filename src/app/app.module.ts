52f998a67772a092f07aaf1184a18364016465c5
Elad Bezalel <elad.bezalel@gmail.com>
2018-04-01 21:53:28 +0300

    Step 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { OverlayModule } from '@angular/cdk/overlay';
@@ -15,6 +16,7 @@ import { ColorPickerTriggerDirective } from './color-picker/color-picker-trigger
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     OverlayModule
   ],
   providers: [],
