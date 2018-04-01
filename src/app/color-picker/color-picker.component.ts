d393cff0a36a87c3b523cc028081c04fac7148da
Elad Bezalel <elad.bezalel@gmail.com>
2018-04-01 21:53:28 +0300

    Step 6

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,10 +1,25 @@
 import {Component, EventEmitter, Input, Output, TemplateRef, ViewChild} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.css'],
-  exportAs: 'colorPicker'
+  exportAs: 'colorPicker',
+  animations: [
+    trigger('picker', [
+      state('void', style({
+        transform: 'scale(0)',
+        opacity: 0
+      })),
+      transition('void <=> *', [
+        style({
+          opacity: 1
+        }),
+        animate('150ms cubic-bezier(0.25, 0.8, 0.25, 1)')
+      ])
+    ])
+  ]
 })
 export class ColorPickerComponent {
   @Input() colors: object[];
