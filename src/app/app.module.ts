4a27d2ebc856fcb1d60b8c3c02097cf3902f5b7b
Elad Bezalel <elad.bezalel@gmail.com>
2018-04-06 19:04:46 +0300

    Step 8 a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { OverlayModule } from '@angular/cdk/overlay';
 import { BidiModule } from '@angular/cdk/bidi';
+import { A11yModule} from '@angular/cdk/a11y';
 
 import { AppComponent } from './app.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { ColorPickerTriggerDirective } from './color-picker/color-picker-trigger.directive';
+import { ColorComponent } from './color-picker/color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColorPickerComponent,
-    ColorPickerTriggerDirective
+    ColorPickerTriggerDirective,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     OverlayModule,
-    BidiModule
+    BidiModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
